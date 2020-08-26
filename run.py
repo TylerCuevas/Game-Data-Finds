a2f2b6106eb4f9f2664581a26c907261d9cb1431
repl.it user <replituser@example.com>
2020-08-26 19:06:43 +0000

    sepersting colums

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
 import pandas
 df= pandas.read_csv("multiTimeline.csv")
 print(df)
+
+
+#Creating a column 
+search_intel = df.Intel_Core
+Week = df.Week
+
+#Printing column
+print(search_intel)
+print(Week)
+
+#The challenge
+search_intel_number = search_intel[0]
+print(search_intel_number)
+
