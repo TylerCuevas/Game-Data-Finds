04be97ce8a13baab6f78a27b10205b95bb9045c9
repl.it user <replituser@example.com>
2020-08-27 19:08:26 +0000

    Added graph

--- run.py
+++ run.py
@@ -1,17 +1,28 @@
 import pandas
+import matplotlib.pyplot as plt
+
 df= pandas.read_csv("multiTimeline.csv")
 print(df)
 
 
 #Creating a column 
-search_intel = df.Intel_Core
+search_intel = df.Nvidia
 Week = df.Week
 
 #Printing column
 print(search_intel)
 print(Week)
 
-#The challenge
-search_intel_number = search_intel[0]
-print(search_intel_number)
+#For plotting data
+plt.plot(Week,search_intel)
+
+plt.xlabel("Week")
+plt.ylabel("Amount of people searching")
+plt.title("Nvidia searches over time")
+
+plt.show()
+
+
+
+
 
